4f624c455952ea61e05be584c7049039317d6bb1
Sallo0 <simiklassnik@yandex.ru>
2022-05-17 06:20:21 +0500

    Рефакториг 2: Возвращение легенды

--- src/main/java/robots/gui/abstractmenu/DialogGenerator.java
+++ src/main/java/robots/gui/abstractmenu/DialogGenerator.java
@@ -14,55 +14,51 @@ public class DialogGenerator {
             ResourceBundle.getBundle("locale").getString("text.no")
     };
 
+
+    private int dialogTemplate(String title, String text, EventObject eventObject) {
+        return JOptionPane.showOptionDialog(
+                ((Component) eventObject.getSource()),
+                text,
+                title,
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+    }
+
     public void changeLocale() {
         options[0] = ResourceBundle.getBundle("locale").getString("text.yes");
         options[1] = ResourceBundle.getBundle("locale").getString("text.no");
     }
 
     public int appExitDialogResult(EventObject eventObject) {
-        return JOptionPane.showOptionDialog(
-                ((Component) eventObject.getSource()),
-                ResourceBundle.getBundle("locale").getString("text.exitAsk"),
+        return dialogTemplate(
                 ResourceBundle.getBundle("locale").getString("title.confirmExit"),
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options,
-                options[0]
+                ResourceBundle.getBundle("locale").getString("text.exitAsk"),
+                eventObject
         );
     }
 
     public int windowsRecoverDialogResult(EventObject eventObject) {
-        return JOptionPane.showOptionDialog(
-                ((Component) eventObject.getSource()),
-                ResourceBundle.getBundle("locale").getString("text.winRestoreAsk"),
+        return dialogTemplate(
                 ResourceBundle.getBundle("locale").getString("title.confirmWinRestore"),
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options,
-                options[0]
+                ResourceBundle.getBundle("locale").getString("text.winRestoreAsk"),
+                eventObject
         );
     }
 
     public void windowExitDialog(EventObject eventObject) {
-        int n = JOptionPane.showOptionDialog(
-                ((Component) eventObject.getSource()),
-                ResourceBundle.getBundle("locale").getString("text.closeWindowAsk"),
+        int dialogResult = dialogTemplate(
                 ResourceBundle.getBundle("locale").getString("text.confirm"),
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options,
-                options[0]
+                ResourceBundle.getBundle("locale").getString("text.closeWindowAsk"),
+                eventObject
         );
-
-        if (n == 0) {
-
+        if (dialogResult == JOptionPane.YES_OPTION) {
             if ((eventObject.getSource()).getClass() == LogWindow.class) {
                 ((Component) eventObject.getSource()).setVisible(false);
             }
-
             if ((eventObject.getSource()).getClass() == GameWindow.class) {
                 ((Component) eventObject.getSource()).setVisible(false);
             }
